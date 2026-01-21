0e7eedbb886c1325b98c55d6304ad2bfc7169793
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-01-21 13:16:15 +0100

    backend

--- task_manager/frontend/script.js
+++
@@ -1,4 +0,0 @@
-fetch("/api/items/")
-fetch("/api/items/", { method: "POST", ... })
-fetch("/api/items/1", { method: "PATCH" })
-fetch("/api/items/1", { method: "DELETE" })
--- task_manager/models/todo.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class TodoCreate(BaseModel):
-    task: str
-
-class Todo(TodoCreate):
-    id: int
-    done: bool = False
