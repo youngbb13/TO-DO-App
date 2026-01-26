c8c2bfbe7257bbe7c1bb8179832a86c786445ed7
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-01-26 22:24:58 +0100

    backend

--- static/script.js
+++ static/script.js
@@ -3,21 +3,32 @@ const newTaskContainer = document.getElementById('new-task-container');
 const saveBtn = document.getElementById('save-btn');
 const newTaskInput = document.getElementById('new-task-input');
 const taskList = document.getElementById('task-list');
-const taskEdit = document.getElementById('task-edit')
-const taskDelete = document.getElementById('task-delete')
+
+//
+const API_BASE = "/api/todos";
+
+window.addEventListener("load", loadTasks);
 
 // Кнопка нажимаєтсья і можна вписати завдання
-addBtn.addEventListener('click', () => {
+addBtn.addEventListener("click", () => {
     newTaskContainer.style.display = 'flex';
+    newTaskInput.focus();
 });
 
-// Відповідає за збереження завдання
-saveBtn.addEventListener('click', () => {
-    const taskText = newTaskInput.value.trim();
-    if(taskText !== '') {
-        const li = document.createElement('li');
-        li.textContent = taskText;
-        taskList.appendChild(li);
-        newTaskInput.value = ''; // очищаємо поле після збереження
+//
+saveBtn.addEventListener("click", addTask);
+
+//
+async function loadTasks() {
+    try {
+        const res = await fetch(API_BASE);
+        if (!res.ok) throw new Error("Failed to load tasks!")
+        const todos = await res.json();
+        taskList.innerHTML = "";
+        todos.forEach(renderTodo);
+    } catch (err) {
+        console.error(err)
+        alert("Error loading list!");
     }
-});
+}
+
